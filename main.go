edc8e0784ad5def64c1639bf70103ed39c118ea6
RaptorExTolop <jamesappletonret@gmail.com>
2024-11-20 20:22:17 +1030

    Dev 0.0.2: finished setting up project

--- main.go
+++ main.go
@@ -1,7 +1,42 @@
 package main
 
-import "fmt"
+import rl "github.com/gen2brain/raylib-go/raylib"
+
+const ()
+
+var (
+	screenwidth  int32 = 1280
+	screenHeight int32 = 720
+
+	running   bool     = true
+	bkgColour rl.Color = rl.Lime
+)
+
+func input() {}
+
+func update() {
+	running = !rl.WindowShouldClose()
+}
+
+func draw() {
+	rl.BeginDrawing()
+
+	rl.ClearBackground(bkgColour)
+
+	rl.EndDrawing()
+}
+
+func init() {
+	rl.InitWindow(screenwidth, screenHeight, "IT'S PONG TIME")
+	rl.SetTargetFPS(60)
+}
 
 func main() {
-	fmt.Println("hello, world!")
+	defer rl.CloseWindow()
+
+	for running {
+		input()
+		update()
+		draw()
+	}
 }
